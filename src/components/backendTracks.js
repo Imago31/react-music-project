83efa611dd00eadc97d2f3fdf38a93dd5d39f205
Valentina <valentina.pg@mail.ru>
2022-08-03 17:06:54 +0300

    ValentinaRodi--add Tracks

---
+++ src/components/backendTracks.js
@@ -0,0 +1,57 @@
+const tracks = [{
+    id: 1,
+    track: 'Song1',
+    singer: 'Singer1',
+    albom: 'Albom',
+    time: '1234',
+},{
+    id: 2,
+    track: 'Song2',
+    singer: 'Singer2',
+    albom: 'Albom2',
+    time: '1234',
+},{
+    id: 3,
+    track: 'Song3',
+    singer: 'Singer3',
+    albom: 'Albom3',
+    time: '1234',
+},{
+    id: 4,
+    track: 'Song3',
+    singer: 'Singer3',
+    albom: 'Albom3',
+    time: '1234',
+},{
+    id: 5,
+    track: 'Song3',
+    singer: 'Singer3',
+    albom: 'Albom3',
+    time: '1234',
+},{
+    id: 6,
+    track: 'Song3',
+    singer: 'Singer3',
+    albom: 'Albom3',
+    time: '1234',
+},{
+    id: 7,
+    track: 'Song3',
+    singer: 'Singer3',
+    albom: 'Albom3',
+    time: '1234',
+},{
+    id: 8,
+    track: 'Song3',
+    singer: 'Singer3',
+    albom: 'Albom3',
+    time: '1234',
+},{
+    id: 9,
+    track: 'Song3',
+    singer: 'Singer3',
+    albom: 'Albom3',
+    time: '1234',
+}]
+
+export default tracks
--- src/components/loginPage/LoginPage.js
+++ src/components/loginPage/LoginPage.js
@@ -1,10 +1,9 @@
 import  './loginPage.css'
 
-
 export default function LoginPage() {
     return (
         <div>
-       <h1>Hello React</h1>
+            <h1>Hello React</h1>
         </div>
     )
   }
