81371c45768ae6189e736683eb00498daddb599c
LazarevEV <lazarev.evgeniy.nf@gmail.com>
2022-09-26 23:04:44 +0300

    Added: Shuffle & Loop

--- src/components/childComponents/player/Player.js
+++ src/components/childComponents/player/Player.js
@@ -26,7 +26,7 @@ export default function Player(props) {
     // Playing States
     const [isPlaying, setIsPlaying] = useState(false)
     const [isLoopEnabled, setIsLoopEnabled] = useState(false)
-    const [isShuffleEnabled, setIsShuffleEnabled] = useState(false)
+    const [isShuffleEnabled, setIsShuffleEnabled] = useState(props.isShuffleEnabled)
     // Current Audio
     const [currentAudio, setCurrentAudio] = useState(new Audio(props.currentTrack.path))
     // Current Track States
@@ -38,6 +38,15 @@ export default function Player(props) {
     const [volume, setVolume] = useState(DEFAULT_VOLUME)
     const [isMuted, setIsMuted] = useState(false)
     const volumeSliderRef = useRef(null)
+
+    // Loop & Shuffle
+    useEffect(() => {
+        setIsLoopEnabled(props.isLoopEnabled)
+    }, [props.isLoopEnabled])
+
+    useEffect(() => {
+        setIsShuffleEnabled(props.isShuffleEnabled)
+    }, [props.isShuffleEnabled])
 
     // Volume
     const getVolumeIcon = () => {
@@ -85,16 +94,13 @@ export default function Player(props) {
     }
 
     const playPrevTrack = () => {
-        if (currTrackHasAdjacent.left) {
-            props.setCurrentTrackByIdx(currentTrackIdx - 1)
-        }
+        if (currTrackHasAdjacent.left) props.setCurrentTrackByIdx(currentTrackIdx - 1)
     }
 
     const playNextTrack = () => {
         console.log('> playNextTrack')
-        if (currTrackHasAdjacent.right) {
-            props.setCurrentTrackByIdx(currentTrackIdx + 1)
-        }
+        if (currTrackHasAdjacent.right) props.setCurrentTrackByIdx(currentTrackIdx + 1)
+        else if (isLoopEnabled) props.setCurrentTrackByIdx(0)
     }
 
     const handleTimeUpdate = () => {
@@ -127,7 +133,7 @@ export default function Player(props) {
         currentAudio.load()
         currentAudio.play()
         currentAudio.addEventListener('timeupdate', handleTimeUpdate)
-    }, [props.currentTrack])
+    }, [props.currentTrack, props.currentTrackIdx])
 
 
     return (
@@ -159,8 +165,18 @@ export default function Player(props) {
                 <span className="song__timeline-text">{secondsToString(Math.floor(currTrackFactLength))} / {secondsToString(currentTrack.duration_in_seconds)}</span>
             </div>
             <div className="player-order__control">
-                <img className="player__icon icon_padding" src={isLoopEnabled ? LoopEnabledIcon : LoopDisabledIcon} alt="Previous Song"></img>
-                <img className="player__icon icon_padding" src={isShuffleEnabled ? ShuffleEnabledIcon : ShuffleDisabledIcon} alt="Previous Song"></img>
+                <img
+                    className="player__icon icon_padding"
+                    src={isLoopEnabled ? LoopEnabledIcon : LoopDisabledIcon}
+                    alt="Previous Song"
+                    onClick={() => setIsLoopEnabled(!isLoopEnabled)}
+                ></img>
+                <img
+                    className="player__icon icon_padding"
+                    src={isShuffleEnabled ? ShuffleEnabledIcon : ShuffleDisabledIcon}
+                    alt="Previous Song"
+                    onClick={() => props.setIsShuffleEnabled(!isShuffleEnabled)}
+                ></img>
                 <img
                     className="player__icon volume"
                     src={getVolumeIcon()}
